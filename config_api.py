d1b57c77528a620639341f041178216dd321a240
yiiner <gerile17@163.com>
2024-06-27 11:13:33 +0800

    6/27 update TODO: evenness color scheme

--- config_api.py
+++ config_api.py
@@ -4,7 +4,7 @@ import os
 os.environ["http_proxy"] = "http://localhost:7890"
 os.environ["https_proxy"] = "http://localhost:7890"
 
-# 6.25
+# 6.27
 client = OpenAI(
     base_url='https://api.openai-proxy.org/v1',
     api_key='',
